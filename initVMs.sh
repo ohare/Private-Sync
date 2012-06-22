66d82e4ba8d1868dedbbd5d7d152241278c6c7c9
Calum O'Hare <calum@iHare.local>
2012-06-22 16:23:58 +1200

    Fixed networking set up added close vms script

--- initVMs.sh
+++ initVMs.sh
@@ -1,1 +1,11 @@
+VBoxManage modifyvm "Lubuntu-One" --nic2 intnet
+VBoxManage modifyvm "Lubuntu-One" --intnet2 "intnet"
 VBoxManage startvm "Lubuntu-One"
+VBoxManage modifyvm "Lubuntu-Two" --nic1 intnet
+VBoxManage modifyvm "Lubuntu-Two" --intnet1 "intnet"
+VBoxManage modifyvm "Lubuntu-Two" --nic2 intnet
+VBoxManage modifyvm "Lubuntu-Two" --intnet2 "intnet2"
+VBoxManage startvm "Lubuntu-Two"
+VBoxManage modifyvm "Lubuntu-Three" --nic1 intnet
+VBoxManage modifyvm "Lubuntu-Three" --intnet1 "intnet2"
+VBoxManage startvm "Lubuntu-Three"
