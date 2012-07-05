85a2c13f0a7c1384b113d4fc1fba6af5ae1ac8fc
Calum <cal@uHare-Two>
2012-07-06 01:11:11 +1200

    watchandsync with unnecessary watching code commented

--- watchandsync.py
+++ watchandsync.py
@@ -8,17 +8,38 @@ class MyEventHandler(pyinotify.ProcessEvent):
         print "Create:",event.pathname
     def process_IN_DELETE(self, event):
         print "Delete:",event.pathname
+        #Can be done with flags...
+        """
+        i = 0
+        rw = open('./folderstowatch','r+')
+        for folder in rw:
+            print folder
+            print i
+            if(folder == event.pathname):
+                #rw.seek(-1 * len(folder),1)
+                rw.seek(i)
+                rw.write("!#!")
+                wm.del_watch(event.pathname)
+                break
+            i += len(folder)
+        rw.close()
+        """
+
     def process_IN_CREATE(self, event):
         print "Modify:",event.pathname
         if os.path.isdir(event.pathname):
             print "Watching: ",event.pathname
-            wm.add_watch(event.pathname,pyinotify.ALL_EVENTS, rec=True)
+            #wm.add_watch(event.pathname,pyinotify.ALL_EVENTS, rec=True)
+            #w = open('./folderstowatch','a')
+            #w.write(event.pathname)
+            #w.close()
 
 def main():
 
-    f = open('./folderstowatch','r')
+    f = open('./folderstowatch','r+')
+
     for folder in f:
-        wm.add_watch(folder.rstrip(),pyinotify.ALL_EVENTS, rec=True)
+        wm.add_watch(folder.rstrip(),pyinotify.ALL_EVENTS, rec=True, auto_add=True)
         print "Watching: ", folder.rstrip()
 
 
