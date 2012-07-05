dfacb43ea0a8617ca5e71ed02e63499edc93cf49
Calum <cal@uHare-Two>
2012-07-06 01:27:50 +1200

    subprocess etc working

--- watchandsync.py
+++ watchandsync.py
@@ -1,7 +1,11 @@
 import pyinotify
 import os
+import subprocess
 
 wm = pyinotify.WatchManager()
+watchedfolders = []
+
+#TODO stats and service and run command
 
 class MyEventHandler(pyinotify.ProcessEvent):
     def process_IN_CREATE(self, event):
@@ -12,7 +16,12 @@ class MyEventHandler(pyinotify.ProcessEvent):
         print "Modify:",event.pathname
         if os.path.isdir(event.pathname):
             print "Watching: ",event.pathname
+        for folder in watchedfolders:
+            if folder in event.pathname:
+                subprocess.call(["ls","-l",folder])
 
+
+#class watchfolders():
 def main():
 
     f = open('./folderstowatch','r')
@@ -20,6 +29,7 @@ def main():
     for folder in f:
         wm.add_watch(folder.rstrip(),pyinotify.ALL_EVENTS, rec=True, auto_add=True)
         print "Watching: ", folder.rstrip()
+        watchedfolders.append(folder.rstrip())
 
 
     eh = MyEventHandler()
