49a2d204416b6d9b108475955eed9ee79cb820b1
Calum O'Hare <calum@iHare.local>
2012-07-09 23:04:09 +1200

    added aims and obj and began on presentation

--- closeVMs.sh
+++ closeVMs.sh
@@ -1,3 +1,5 @@
 VBoxManage controlvm "Lubuntu-One" poweroff
 VBoxManage controlvm "Lubuntu-Two" poweroff
 VBoxManage controlvm "Lubuntu-Three" poweroff
+VBoxManage controlvm "Lubuntu-Four" poweroff
+VBoxManage controlvm "Lubuntu-Five" poweroff
--- initVMs.sh
+++ initVMs.sh
@@ -8,4 +8,14 @@ VBoxManage modifyvm "Lubuntu-Two" --intnet2 "intnet2"
 VBoxManage startvm "Lubuntu-Two"
 VBoxManage modifyvm "Lubuntu-Three" --nic1 intnet
 VBoxManage modifyvm "Lubuntu-Three" --intnet1 "intnet2"
+VBoxManage modifyvm "Lubuntu-Three" --nic2 intnet
+VBoxManage modifyvm "Lubuntu-Three" --intnet2 "intnet3"
 VBoxManage startvm "Lubuntu-Three"
+VBoxManage modifyvm "Lubuntu-Four" --nic1 intnet
+VBoxManage modifyvm "Lubuntu-Four" --intnet1 "intnet3"
+VBoxManage modifyvm "Lubuntu-Four" --nic2 intnet
+VBoxManage modifyvm "Lubuntu-Four" --intnet2 "intnet4"
+VBoxManage startvm "Lubuntu-Four"
+VBoxManage modifyvm "Lubuntu-Five" --nic1 intnet
+VBoxManage modifyvm "Lubuntu-Five" --intnet1 "intnet4"
+VBoxManage startvm "Lubuntu-Five"
