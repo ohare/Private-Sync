1eba660fc28d952d274cb115e98b8f298ec4595a
Calum O'Hare <calum@iHare.local>
2012-07-20 01:38:00 +1200

    Interim report coming along

--- initVMs.sh
+++ initVMs.sh
@@ -1,21 +1,22 @@
-VBoxManage modifyvm "Lubuntu-One" --nic2 intnet
-VBoxManage modifyvm "Lubuntu-One" --intnet2 "intnet"
-VBoxManage startvm "Lubuntu-One"
-VBoxManage modifyvm "Lubuntu-Two" --nic1 intnet
-VBoxManage modifyvm "Lubuntu-Two" --intnet1 "intnet"
-VBoxManage modifyvm "Lubuntu-Two" --nic2 intnet
-VBoxManage modifyvm "Lubuntu-Two" --intnet2 "intnet2"
-VBoxManage startvm "Lubuntu-Two"
-VBoxManage modifyvm "Lubuntu-Three" --nic1 intnet
-VBoxManage modifyvm "Lubuntu-Three" --intnet1 "intnet2"
-VBoxManage modifyvm "Lubuntu-Three" --nic2 intnet
-VBoxManage modifyvm "Lubuntu-Three" --intnet2 "intnet3"
-VBoxManage startvm "Lubuntu-Three"
-VBoxManage modifyvm "Lubuntu-Four" --nic1 intnet
-VBoxManage modifyvm "Lubuntu-Four" --intnet1 "intnet3"
-VBoxManage modifyvm "Lubuntu-Four" --nic2 intnet
-VBoxManage modifyvm "Lubuntu-Four" --intnet2 "intnet4"
-VBoxManage startvm "Lubuntu-Four"
-VBoxManage modifyvm "Lubuntu-Five" --nic1 intnet
-VBoxManage modifyvm "Lubuntu-Five" --intnet1 "intnet4"
-VBoxManage startvm "Lubuntu-Five"
+VBoxManage modifyvm "Ubuntu-Test" --nic2 intnet
+VBoxManage modifyvm "Ubuntu-Test" --intnet2 "intnet"
+VBoxManage startvm "Ubuntu-Test"
+VBoxManage modifyvm "Ubuntu-Test2" --nic1 intnet
+VBoxManage modifyvm "Ubuntu-Test2" --intnet1 "intnet"
+VBoxManage modifyvm "Ubuntu-Test2" --nic2 intnet
+VBoxManage modifyvm "Ubuntu-Test2" --intnet2 "intnet2"
+VBoxManage startvm "Ubuntu-Test2"
+VBoxManage modifyvm "Ubuntu-Test3" --nic1 intnet
+VBoxManage modifyvm "Ubuntu-Test3" --intnet1 "intnet2"
+VBoxManage modifyvm "Ubuntu-Test3" --nic2 intnet
+VBoxManage modifyvm "Ubuntu-Test3" --intnet2 "intnet3"
+VBoxManage startvm "Ubuntu-Test3"
+VBoxManage modifyvm "Ubuntu-Test4" --nic1 intnet
+VBoxManage modifyvm "Ubuntu-Test4" --intnet1 "intnet3"
+VBoxManage modifyvm "Ubuntu-Test4" --nic2 intnet
+VBoxManage modifyvm "Ubuntu-Test4" --intnet2 "intnet4"
+VBoxManage startvm "Ubuntu-Test4"
+#VBoxManage modifyvm "Ubuntu-Test5" --nic1 intnet
+#VBoxManage modifyvm "Ubuntu-Test5" --intnet1 "intnet4"
+#VBoxManage startvm "Ubuntu-Test5"
+#VBoxManage dhcpserver add --netname intnet --ip 192.168.4.0 --netmask 255.255.255.0 --lowerip 192.168.4.1 --upperip 192.168.4.10
