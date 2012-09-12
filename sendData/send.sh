630dfc1ffb58e98ead61587135bb9e6e564a044e
ohare <calum.ohare@gmail.com>
2012-09-13 01:08:41 +1200

    Added send shell script to send random files to vms

---
+++ sendData/send.sh
@@ -0,0 +1,5 @@
+scp -p rten cal@192.168.0.26:/home/cal/Documents/round/
+sleep 10
+scp -p rten2 cal@192.168.0.26:/home/cal/Documents/round/
+sleep 10
+scp -p rten3 cal@192.168.0.26:/home/cal/Documents/round/
