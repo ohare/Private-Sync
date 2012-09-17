8c6157d9d8a89a928ab2b462dad3a604c5aab48c
ohare <calum.ohare@gmail.com>
2012-09-18 01:05:40 +1200

    Minor

--- watchandsync.py
+++ watchandsync.py
@@ -166,8 +166,8 @@ class MyEventHandler(pyinotify.ProcessEvent):
                         if args.scp:
                             print "scp","-rp",folder,ip + ":/tmp/" + fname
                             subprocess.call(["scp","-rp",folder,ip + ":/tmp/" + fname])
-                            print "ssh",ip,"find /tmp/" + fname + " -type f -exec mv -iv {} " + path + fname + "/ \;"
-                            subprocess.call(["ssh",ip,"find /tmp/" + fname + " -type f -exec mv -iv {} " + path + fname + "/ \;"])
+                            print "ssh",ip,"yes y | find /tmp/" + fname + " -type f -exec mv -iv {} " + path + fname + "/ \;"
+                            subprocess.call(["ssh",ip,"yes y | find /tmp/" + fname + " -type f -exec mv -iv {} " + path + fname + "/ \;"])
                         elif args.rsync:
                             print "rsync","-rt",folder,ip + ":" + path
                             subprocess.call(["rsync","-rt",folder,ip + ":" + path])
