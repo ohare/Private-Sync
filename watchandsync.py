fb21a71edea413ed74ed967bcbafdacf669e51c7
ohare <calum.ohare@gmail.com>
2012-09-20 02:55:40 +1200

    Minor

--- watchandsync.py
+++ watchandsync.py
@@ -23,7 +23,7 @@ class Tools():
 
     def timeElapsed(self, dtstamp, diff):
         FMT = '%H:%M:%S.%f'
-        tdelta = datetime.datetime.now() - datetime.strptime(dtstamp, FMT)
+        tdelta = datetime.datetime.now() - datetime.datetime.strptime(dtstamp, FMT)
         print  tdelta.total_seconds()
         if tdelta.total_seconds() >= diff:
             print "Time perioed reached"
