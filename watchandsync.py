e771e27ff9baea3f981393971518341b98e6a966
ohare <calum.ohare@gmail.com>
2012-09-20 12:28:32 +1200

    Minor

--- watchandsync.py
+++ watchandsync.py
@@ -258,9 +258,8 @@ def main():
                         watchedfolders[info[0].rstrip()] = []
                     watchedfolders[info[0].rstrip()].append(info[1])
                     watchedfolders[info[0].rstrip()].append(info[2])
-                    if watchedfolders[info[0].rstrip()][3]:
-                        watchedfolders[info[0].rstrip()].append(info[3])
-                        watchedfolders[info[0].rstrip()].append(str(datetime.datetime.min))
+                    watchedfolders[info[0].rstrip()].append(info[3])
+                    watchedfolders[info[0].rstrip()].append(str(datetime.datetime.min))
                 else:
                     print "Removing: " + info[0]
         f.close()
