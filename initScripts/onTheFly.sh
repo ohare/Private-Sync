082527ff0375f67af5da4de14f885903cb608533
ohare <calum.ohare@gmail.com>
2012-10-03 14:31:41 +1300

    Line unison 10mb 10s 10s wait

--- initScripts/onTheFly.sh
+++ initScripts/onTheFly.sh
@@ -9,10 +9,10 @@ ethcountarr=(1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1)
 incount=1
 bigncount=2
 littlencount=1
-folderpath="/home/cal/Documents/t16"
+folderpath="/home/cal/Documents/t17"
 folderpath2="/home/cal/Documents/t02"
 homepath="/home/cal/Documents/Private-Sync/"
-waitTime=20
+waitTime=10
 
 function clear_ifaces() {
     i=0
